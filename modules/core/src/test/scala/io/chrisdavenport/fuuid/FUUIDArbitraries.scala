b7156839783d8b11f25d1d92e5239d856e4baf87
Chris Davenport <chris@christopherdavenport.tech>
2018-07-24 09:40:14 -0400

    Use gen uuid from scalacheck

--- modules/core/src/test/scala/io/chrisdavenport/fuuid/FUUIDArbitraries.scala
+++ modules/core/src/test/scala/io/chrisdavenport/fuuid/FUUIDArbitraries.scala
@@ -1,17 +1,11 @@
 package io.chrisdavenport.fuuid
 
 import org.scalacheck._
-import java.util.UUID
 
 trait FUUIDArbitraries {
-  private implicit val arbUuid: Arbitrary[UUID] = Arbitrary( for {
-    l1 <- Gen.choose(Long.MinValue, Long.MaxValue)
-    l2 <- Gen.choose(Long.MinValue, Long.MaxValue)
-    y <- Gen.oneOf('8', '9', 'a', 'b')
-  } yield UUID.fromString(new UUID(l1,l2).toString.updated(14, '4').updated(19, y)))
 
   implicit val arbFUUID: Arbitrary[FUUID] = Arbitrary(
-    Arbitrary.arbitrary[UUID].map(FUUID.fromUUID)
+    Gen.uuid.map(FUUID.fromUUID)
   )
 
   implicit val arbFUUIDFunction: Arbitrary[FUUID => FUUID] = Arbitrary(
