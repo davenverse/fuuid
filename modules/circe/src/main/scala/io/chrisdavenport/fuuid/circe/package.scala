d187fc9eb1dbf125f021bdf629533841f3049b16
Vladislav Pakhomov <d1skortneru@yandex.ru>
2018-07-25 16:42:33 +0300

    use show

--- modules/circe/src/main/scala/io/chrisdavenport/fuuid/circe/package.scala
+++ modules/circe/src/main/scala/io/chrisdavenport/fuuid/circe/package.scala
@@ -1,10 +1,11 @@
 package io.chrisdavenport.fuuid
 
+import cats.syntax.show._
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder}
 
 package object circe {
-  implicit val encoder: Encoder[FUUID] = Encoder.instance(_.toString.asJson)
+  implicit val encoder: Encoder[FUUID] = Encoder.instance(_.show.asJson)
   implicit val decoder: Decoder[FUUID] =
     Decoder[String].emap(FUUID.fromString(_).left.map(_.toString))
 }
