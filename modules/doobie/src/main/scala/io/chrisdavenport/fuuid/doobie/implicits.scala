a304c422c531d2e587b8dc282af59f92a1069930
Michael Pilquist <mpilquist@gmail.com>
2018-09-28 08:08:52 -0400

    Fixed doobie compilation errors

--- modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
+++ modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
@@ -2,11 +2,11 @@ package io.chrisdavenport.fuuid.doobie
 
 import java.util.UUID
 
-import doobie.util.meta.Meta
+import doobie.util.Meta
 import io.chrisdavenport.fuuid.FUUID
 
 object implicits {
 
   implicit def FuuidType(implicit U: Meta[UUID]): Meta[FUUID] =
-    U.xmap[FUUID](FUUID.fromUUID, fuuid => FUUID.Unsafe.toUUID(fuuid))
+    U.timap[FUUID](FUUID.fromUUID)(fuuid => FUUID.Unsafe.toUUID(fuuid))
 }
