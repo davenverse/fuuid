40202ad179decaef460ac4206746a0f8a366f4f6
JesusMtnez <jesusmartinez93@gmail.com>
2021-05-18 09:44:15 +0200

    Fix new FuuidMeta

--- modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
+++ modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
@@ -3,14 +3,19 @@ package io.chrisdavenport.fuuid.doobie
 import java.util.UUID
 
 import doobie.Meta
+import doobie.util.{Get, Put}
 import io.chrisdavenport.fuuid.FUUID
 
 object implicits {
 
   @deprecated(message = "Please use FuuidMeta instead.", since = "0.5.1")
   def FuuidType(implicit U: Meta[UUID]): Meta[FUUID] =
-    FuuidMeta(U, U)
+    FuuidMeta(Get[FUUID], Put[FUUID])
 
-  implicit def FuuidMeta(implicit A: Get[UUID], B: Put[UUID]): Meta[FUUID] =
-    U.timap[FUUID](FUUID.fromUUID)(fuuid => FUUID.Unsafe.toUUID(fuuid))
+  implicit def FuuidGet(implicit G: Get[UUID]): Get[FUUID] = G.map(FUUID.fromUUID)
+
+  implicit def FuuidPut(implicit P: Put[UUID]): Put[FUUID] = P.contramap(FUUID.Unsafe.toUUID)
+
+  implicit def FuuidMeta(implicit A: Get[FUUID], B: Put[FUUID]): Meta[FUUID] = Meta[FUUID]
+
 }
