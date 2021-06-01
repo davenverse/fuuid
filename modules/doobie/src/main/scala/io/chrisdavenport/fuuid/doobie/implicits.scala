75a4cdec62eccf534ca3f0f2af4be6058246b9ed
David Strawn <isomarcte@gmail.com>
2021-06-01 11:33:03 -0600

    Scalafmt

--- modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
+++ modules/doobie/src/main/scala/io/chrisdavenport/fuuid/doobie/implicits.scala
@@ -8,7 +8,10 @@ import io.chrisdavenport.fuuid.FUUID
 
 object implicits {
 
-  @deprecated(message = "You should not demand a Meta implicitly. Please use FuuidGet or FuuidPut instead.", since = "0.5.1")
+  @deprecated(
+    message = "You should not demand a Meta implicitly. Please use FuuidGet or FuuidPut instead.",
+    since = "0.5.1"
+  )
   def FuuidType(implicit U: Meta[UUID]): Meta[FUUID] =
     FuuidMeta(Get[FUUID], Put[FUUID])
 
@@ -16,7 +19,10 @@ object implicits {
 
   implicit def FuuidPut(implicit P: Put[UUID]): Put[FUUID] = P.contramap(FUUID.Unsafe.toUUID)
 
-  @deprecated(message = "You should not demand a Meta implicitly. Please use FuuidGet or FuuidPut instead.", since = "0.6.1")
+  @deprecated(
+    message = "You should not demand a Meta implicitly. Please use FuuidGet or FuuidPut instead.",
+    since = "0.6.1"
+  )
   def FuuidMeta(implicit A: Get[FUUID], B: Put[FUUID]): Meta[FUUID] =
     new Meta(A, B)
 }
