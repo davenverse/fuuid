9350954ad0dbd00b6b98608f2cdf201b29aa8912
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2021-06-02 06:07:39 +0200

    Add circe KeyDecoder/KeyEncoder for FUUID (#340)

--- modules/circe/src/test/scala/io/chrisdavenport/fuuid/FUUIDSerdeSpec.scala
+++ modules/circe/src/test/scala/io/chrisdavenport/fuuid/FUUIDSerdeSpec.scala
@@ -1,6 +1,7 @@
 package io.chrisdavenport.fuuid
 
 import io.chrisdavenport.fuuid.circe._
+import io.circe.{KeyDecoder, KeyEncoder}
 import io.circe.syntax._
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
@@ -9,6 +10,12 @@ class FUUIDSerdeSpec extends Specification with ScalaCheck with FUUIDArbitraries
   "circe serialization and deserialization" should {
     "correct serialize and deserialize" in prop { validFUUID: FUUID =>
       validFUUID.asJson.as[FUUID] must beRight.like { case fuuid =>
+        fuuid === validFUUID
+      }
+    }
+
+    "correct key serialize and deserialize" in prop { validFUUID: FUUID =>
+      KeyDecoder[FUUID].apply(KeyEncoder[FUUID].apply(validFUUID)) must beSome.like { case fuuid =>
         fuuid === validFUUID
       }
     }
