8c8c749dfeac7aa910313259e90f3066d82d8ddd
Brandon Brown <brandon@bbrownsound.com>
2021-06-10 14:13:09 -0400

    feat(switch to munit): switching from spec2 to munit for testing. #384.

--- modules/core/src/test/scala/io/chrisdavenport/fuuid/FUUIDTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/fuuid/FUUIDTests.scala
@@ -1,13 +1,10 @@
 package io.chrisdavenport.fuuid
 
-import org.specs2.mutable.Specification
-import org.typelevel.discipline.specs2.mutable.Discipline
-import cats.kernel.laws.discipline.HashTests
-import cats.kernel.laws.discipline.OrderTests
+import cats.kernel.laws.discipline.{HashTests, OrderTests}
+import munit.{CatsEffectSuite, DisciplineSuite}
 import cats.implicits._
 
-class FUUIDTests extends Specification with Discipline with FUUIDArbitraries {
-
+class FUUIDTests extends CatsEffectSuite with DisciplineSuite with FUUIDArbitraries {
   checkAll("FUUID", HashTests[FUUID].hash)
   checkAll("FUUID", OrderTests[FUUID].order)
 }
